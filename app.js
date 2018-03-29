5e5d9d16744863dc8e4b63f781ad13fe8ecba6b7
kolaveridi <coderboysatya@gmail.com>
2018-03-29 22:17:33 +0530

    updated

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 const express=require('express');
 const app=express();
 const  bodyParser = require('body-parser');
-
-
+const methodOverride = require('method-override')
 const  exphbs  = require('express-handlebars');
 const mongoose=require('mongoose');
-// body parser middleware
+const flash = require('connect-flash');
+const session = require('express-session');
+
+
+// body parser middleware and just use it
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+// method override middleware
+app.use(methodOverride('_method'))
+//expressjs session middleware copied from https://github.com/expressjs/session
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
 
+}))
+//using flash
+app.use(flash());
+app.use(function(req,res,next){
+    res.locals.success_msg=req.flash('success_msg');
+    res.locals.error_msg=req.flash('error_msg');
+    next();
+
+});
+// note that Vidjot-dev is there in mongodb now
 mongoose.connect('mongodb://localhost/VidJot-dev',{
    useMongoClient:true
 }).then(()=> console.log('Mongodb connected............................................'));
@@ -86,6 +106,58 @@ app.post('/ideas',function(req,res){
     .save().
     then(idea=>{res.redirect('/ideas');
      })
+  }
+});
+//https://github.com/expressjs/method-override
+app.put('/ideas/:id',function(req,res){
+  Idea.findOne({
+    _id:req.params.id
+  }).
+  then(idea =>{
+    idea.title=req.body.title;
+    idea.details=req.body.details;
+    idea.save().then(idea=>{
+      res.redirect('/ideas');
+    })
+
+  });
+});
+// delete  ideas
+app.delete('/ideas/:id',function(req,res){
+  Idea.remove({
+  _id:req.params.id
+}).then(()=>{
+
+  res.redirect('/ideas');
+  });
+});
+//User login route
+app.get('/users/login',function(req,res){
+  res.render('users/login');
+});
+// user register route
+app.get('/users/register',function(req,res){
+  res.render('users/register');
+});
+//Register form post
+app.post('/users/register',function(req,res){
+  console.log(req.body);
+  var errors=[];
+  if(req.body.password!=req.body.password2){
+    errors.push({text:'Passswords donott match'});
+  }
+  if(req.body.password.length<4){
+    errors.push({text:'Password must be atleast 4 letter word'});
+}
+  //we don't have to clear the form again even there is anything wrong entered so
+  if(errors.length>0){
+    res.render('users/register',{
+      errors:errors,
+      name:req.body.name,
+      email:req.body.email,
+      password:req.body.password,
+      password2:req.body.password2
+    });
   }
 });
 app.listen(3000,()=>console.log(`Server started on ${port}`));
