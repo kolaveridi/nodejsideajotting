9f5e30d7d418ca988f882f0fed8682d2dda6425f
kolaveridi <coderboysatya@gmail.com>
2018-03-30 21:42:07 +0530

    updated

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const  exphbs  = require('express-handlebars');
 const mongoose=require('mongoose');
 const flash = require('connect-flash');
 const session = require('express-session');
-
+const bcrypt=require('bcryptjs');
+const passport=require('passport');
+require('./models/User.js');
+const User=mongoose.model('users');
+require('./config/passport')(passport);
 
 // body parser middleware and just use it
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -20,11 +24,16 @@ app.use(session({
   saveUninitialized: true,
 
 }))
+//passport middleware copied
+app.use(passport.initialize());
+app.use(passport.session());
+
 //using flash
 app.use(flash());
 app.use(function(req,res,next){
     res.locals.success_msg=req.flash('success_msg');
     res.locals.error_msg=req.flash('error_msg');
+    res.locals.user=req.user || null;
     next();
 
 });
@@ -135,6 +144,20 @@ app.delete('/ideas/:id',function(req,res){
 app.get('/users/login',function(req,res){
   res.render('users/login');
 });
+// user Logout route
+app.get('/users/logout',function(req,res){
+  req.logout();
+  req.flash('success_msg','You are logged out');
+  res.redirect('/users/login');
+})
+// users login post
+app.post('/users/login',function(req,res,next){
+ passport.authenticate('local', {
+  successRedirect:'/ideas',
+  failureRedirect:'/users/login',
+  fialureFlash:true
+})(req,res,next);
+});
 // user register route
 app.get('/users/register',function(req,res){
   res.render('users/register');
@@ -144,7 +167,7 @@ app.post('/users/register',function(req,res){
   console.log(req.body);
   var errors=[];
   if(req.body.password!=req.body.password2){
-    errors.push({text:'Passswords donott match'});
+    errors.push({text:'Passswords donot match'});
   }
   if(req.body.password.length<4){
     errors.push({text:'Password must be atleast 4 letter word'});
@@ -159,5 +182,31 @@ app.post('/users/register',function(req,res){
       password2:req.body.password2
     });
   }
-});
+  else{
+    // check first if user with this name already exists
+    User.findOne({email:req.body.email})
+    .then(user=>{
+       if(user){
+         req.flash('error_msg','Email already exist');
+         res.redirect('/users/regsiter');
+       }
+     else{
+       const newUser =new User({
+         name:req.body.name,
+         email:req.body.email,
+         password:req.body.password
+       });
+       bcrypt.genSalt(10,(err,salt)=>{
+         bcrypt.hash(newUser.password,salt,(err,hash)=>{
+           if(err) throw err;
+           newUser.password=hash;
+           newUser.save()
+           .then(()=>{
+                  res.redirect('/users/login');
+           });
+         });
+       });
+     }}
+   )}
+ });
 app.listen(3000,()=>console.log(`Server started on ${port}`));
